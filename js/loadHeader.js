a081987035ed4f5f3948ade8d6cb76510f0542e3
JonathanRobinsonAristizabalSoto <jonathan_aristizabal@soy.sena.edu.co>
2024-11-24 01:04:44 -0500

    Co-authored-by: DirlenyOrtiz <DirlenyOrtiz@users.noreply.github.com>

---
+++ js/loadHeader.js
@@ -0,0 +1,19 @@
+// FILE: js/loadHeader.js
+// Cargar el contenido del header
+fetch('partials/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+
+        // Después de cargar el header, inicializa el menú hamburguesa
+        const menuToggle = document.getElementById("menu-toggle");
+        const sidebar = document.getElementById("sidebar");
+
+        if (menuToggle && sidebar) {
+            menuToggle.addEventListener("click", function () {
+                // Toggle para mostrar/ocultar el sidebar
+                sidebar.classList.toggle("active");
+                sidebar.classList.toggle("translate-x-0"); // Muestra/oculta el sidebar
+            });
+        }
+    });
